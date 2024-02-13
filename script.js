73763c9a778ce5d3f6184445398bd399a846fcc5
vinze01 <125807426+vinze01@users.noreply.github.com>
2024-02-14 06:03:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const flowersBtn = document.querySelector(".flowers");
+
+flowersBtn.style.display = "none"; // Hide the flowers button initially
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you! 😘";
+  gif.style.width = "350px";
+  gif.style.height = "700px";
+  gif.src =
+    "https://en.bloggif.com/output/f/1/f1a2b2b8b0db39f73d98f8e7abc34ab7.gif?1707858766";
+
+  // Show the flowers button and hide Yes and No buttons
+  flowersBtn.style.display = "inline-block";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
+flowersBtn.addEventListener("click", () => {
+  const flowersGIF = document.createElement("img");
+  flowersGIF.src = "https://media.giphy.com/media/K51Nva4IXroB2zwaVR/giphy.gif";
+  flowersGIF.width = 400;
+  flowersGIF.height = 480;
+
+  // Remove the existing GIF
+  gif.style.display = "none";
+
+  wrapper.appendChild(flowersGIF);
+
+  flowersBtn.style.display = "none";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
